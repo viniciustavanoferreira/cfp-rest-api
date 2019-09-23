586af04a5b19c3beb4bb4f582e4d0184ced04fc1
Vinicius T. Ferreira <vtf.fatec@hotmail.com>
2019-09-23 17:41:14 -0300

    Create PHP files to get and test connection to MySQL.

---
+++ connection_factory.php
@@ -0,0 +1,18 @@
+<?php
+
+function OpenCon(){
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "controle_financeiro_pessoal";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die ("Connection failed: %s\n". $conn -> error); 
+
+    return $conn;
+}
+
+function CloseCon($conn){
+    $conn -> close();
+}
+
+?>
